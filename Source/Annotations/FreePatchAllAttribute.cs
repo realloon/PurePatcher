912a1d52941b8727bcb3941f27f7e31c8e7e4409
sincerajn <sincerajn@outlook.com>
2026-07-01 02:56:45 +0800

    chore: add MeansImplicitUse annotations to suppress false unused warnings

--- Source/Annotations/FreePatchAllAttribute.cs
+++ Source/Annotations/FreePatchAllAttribute.cs
@@ -1,7 +1,10 @@
+using JetBrains.Annotations;
+
 namespace PurePatcher.Annotations;
 
 /// <summary>
 /// Marks an assembly rewriting method
 /// </summary>
+[MeansImplicitUse]
 [AttributeUsage(AttributeTargets.Method)]
 public class FreePatchAllAttribute : Attribute;
---
+++ Source/Annotations/JetBrainsAnnotations.cs
@@ -0,0 +1,6 @@
+// ReSharper disable once CheckNamespace
+
+namespace JetBrains.Annotations;
+
+// Rider/ReSharper recognizes this exact annotation name for IDE-only implicit-use metadata.
+internal sealed class MeansImplicitUseAttribute : Attribute;
